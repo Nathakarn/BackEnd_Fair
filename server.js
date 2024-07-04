9e81dc789ee157b33381d66edf10e80b5ff16743
nathakarn <nathakarn49@gmail.com>
2024-07-04 09:14:24 +0700

    nuthere

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use('/product', productRoute)
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log("Server on", port));
-<<<<<<< HEAD
-=======
 
 console.log(port);
 console.log("Mix");
@@ -28,4 +26,4 @@ console.log("lolo");
 console.log("Max");
 console.log("Min");
 console.log("lolo");
->>>>>>> 0568186f19600112e907f942a0198f66c23f53f6
+console.log("NutHere!");
