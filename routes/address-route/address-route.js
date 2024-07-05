06eb87185af438631b2b3689345eae834949ade3
Komcharn <93968252+komcharnok@users.noreply.github.com>
2024-07-05 14:42:19 +0700

    clg

--- routes/address-route/address-route.js
+++ routes/address-route/address-route.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const { getallAddress } = require('../../controllers/address-controller/address-controller')
 const { createAddress } = require('../../controllers/address-controller/address-controller')
+const { updateAddress } = require('../../controllers/address-controller/address-controller')
+const { deleteAddress } = require('../../controllers/address-controller/address-controller')
 
 const addressRoute = express.Router()
 
-addressRoute.get('/', createAddress)
+addressRoute.get('/', getallAddress )
+addressRoute.post('/', createAddress)
+addressRoute.put('/', updateAddress)
+addressRoute.delete('/', deleteAddress)
 
 
 module.exports = addressRoute
