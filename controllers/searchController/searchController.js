8c95e47839e68f3320e70be4c661ebbd57ff452d
MixDev <chaiwiwatmixx@gmail.com>
2024-07-15 14:31:18 +0700

    search

--- controllers/searchController/searchController.js
+++ controllers/searchController/searchController.js
@@ -2,24 +2,49 @@ const customError = require("../../utils/customError");
 const tryCatch = require("../../utils/tryCatch");
 const prisma = require("../../models");
 
-const searchProduct = async (req, res) => {
-  const { keyword } = req.query;
+const searchProduct = async (req, res, next) => {
+  try {
+    const { keyword } = req.query;
 
-  if (!keyword) {
-    customError("Please enter keyword", 400);
-  }
+    if (!keyword) {
+      customError("Please enter keyword", 400);
+    }
 
-  const searchResult = await prisma.product.findMany({
-    where: {
-      product_title: {
-        contains: keyword.toLowerCase(),
+    const searchResult = await prisma.product.findMany({
+      where: {
+        product_title: {
+          contains: keyword.toLowerCase(),
+        },
       },
-    },
-  });
+    });
 
-  res.status(200).json(searchResult);
+    res.status(200).json(searchResult);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const searchCategory = async (req, res, next) => {
+  try {
+    const { category } = req.query;
+
+    if (!category) {
+      customError("Please enter category", 400);
+    }
+
+    const searchResult = await prisma.product.findMany({
+      where: {
+        category: category,
+      },
+    });
+
+    res.status(200).json(searchResult);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   searchProduct,
+  searchCategory,
 };
--- routes/searchRoute/searchRoute.js
+++ routes/searchRoute/searchRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {
   searchProduct,
+  searchCategory,
 } = require("../../controllers/searchController/searchController");
 const Route = express.Router();
 
 Route.get("/", searchProduct);
+Route.get("/category", searchCategory);
 
 module.exports = Route;
