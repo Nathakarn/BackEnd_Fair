ed34e886d0142ca5658ffadce4559268c57b4a93
Komcharn <93968252+komcharnok@users.noreply.github.com>
2024-07-17 13:28:21 +0700

    Edit Controller

--- controllers/order-controller/order-controller.js
+++ controllers/order-controller/order-controller.js
@@ -5,8 +5,8 @@ const prisma = require("../../models");
 
 module.exports.getorderById = tryCatch(async (req, res) => {
     const rs = await prisma.order.findMany({
-        where: {user_id : req.user.user_id},
-        include: {product:true}
+        where: { user_id: req.user.user_id },
+        include: { product: true }
     })
     res.json(rs)
 });
@@ -15,24 +15,35 @@ module.exports.getorderById = tryCatch(async (req, res) => {
 module.exports.createOrder = tryCatch(async (req, res) => {
     const { id } = req.params;
     const { quantity } = req.body;
-    console.log("create order", req.params);
-    console.log("create quantity", req.body);
-    console.log(req.user)
-    await prisma.order.create({
-        data: {
-            user_id: req.user.user_id,
-            product_id: +id,  
-            quantity:+quantity.quantity
-        }
+    const productUser = await prisma.order.findMany({
+        where: { user_id: req.user.user_id,product_id:+id }
     })
-
+    if (productUser.length > 0) {
+        const sum = (productUser[0].quantity + quantity)
+        const rs = await prisma.order.updateMany({
+            where: { product_id: +id },
+            data: {
+                quantity: sum
+            }
+        })
+        res.json(rs)
+    } else {
+        const rs = await prisma.order.create({
+            data: {
+                user_id: req.user.user_id,
+                product_id: +id,
+                quantity: +quantity
+            }
+        })
+        res.json(rs)
+    }
 });
 
 
 module.exports.deleteorderById = tryCatch(async (req, res) => {
-    const {id } = req.params;
+    const { id } = req.params;
     const rs = await prisma.order.delete({
-        where:{order_id : +id}
+        where: { order_id: +id }
     })
     console.log(rs)
 
